4d3d642d4bdb71a9b4e0725003fd1271bce3c514
Yokesh Ezumalai <vyash05@bankboubyan.com>
2019-02-21 17:45:59 +0300

    Initial Commit

---
+++ sample/src/main/java/com/yokesh/seekbarwithhint/DemoActivity.kt
@@ -0,0 +1,14 @@
+package com.yokesh.seekbarwithhint
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DemoActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+
+    }
+
+}
